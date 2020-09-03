2c2edc71875417a3e62e2499a5ba396061ff3e1c
Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>
2020-09-03 19:50:39 +0300

    add icon

--- MagicMirror/MagicMirror.Android/MainActivity.cs
+++ MagicMirror/MagicMirror.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MagicMirror.Droid
 {
-    [Activity(Label = "MagicMirror", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MagicMirror", Icon = "@mipmap/mirror", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
