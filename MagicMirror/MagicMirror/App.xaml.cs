3c2df2079faab800c32f1c2311fef84835ae0f19
Дежурнюк Владимир Викторович <v.dezhurnyuk@scout-gps.ru>
2020-09-11 13:02:24 +0300

    replace project

--- MagicMirror/MagicMirror/App.xaml.cs
+++ MagicMirror/MagicMirror/App.xaml.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using MagicMirror.Services;
+using MagicMirror.Views;
 
 namespace MagicMirror
 {
     public partial class App : Application
     {
+
         public App()
         {
             InitializeComponent();
 
+            DependencyService.Register<MockDataStore>();
             MainPage = new MainPage();
         }
 
---
+++ MagicMirror/MagicMirror/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MagicMirror.Models;
+
+namespace MagicMirror.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
